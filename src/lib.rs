1bdd8f65205d1955d1518c456a730ac43d213c3f
Alex Choi <k5j9b7c6fb@privaterelay.appleid.com>
2025-12-07 19:50:01 -0500

    Remove SharedTransactionState (#7)

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,7 @@ pub use yachtsql_core::types::{DataType, Value, collation};
 pub use yachtsql_executor::{QueryExecutor, Table};
 pub use yachtsql_parser::{CustomStatement, DialectType, Parser, Statement};
 pub use yachtsql_storage::{
-    Field, FieldMode, IsolationLevel, Schema, SharedTransactionState, Storage, Transaction,
-    TransactionManager,
+    Field, FieldMode, IsolationLevel, Schema, Storage, Transaction, TransactionManager,
 };
 
 pub mod mvcc {
